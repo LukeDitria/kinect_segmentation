9770ec4b529e9d79e7d12c3f4b612ae5734a3d86
shaynedlima <dlimashayne@gmail.com>
2019-01-08 17:21:25 +1100

    added ellipse centroid fitting

--- src/segment_tabletop.h
+++ src/segment_tabletop.h
@@ -40,8 +40,7 @@
 
 typedef pcl::Histogram<90> CRH90;
 
-//GLOBAL VARIABLES
-float cylinder_offset = 0.025;
+
 
 class SegmentTabletop {
 
@@ -49,7 +48,6 @@ class SegmentTabletop {
   ros::NodeHandle nh_;
   ros::Subscriber point_cloud_sub_;
   ros::Publisher object_markers_pub_;
-  tf::TransformListener transformer;
   tf::TransformListener listener;
   std::string point_cloud_topic;
   std::string out_object_markers_topic;
@@ -167,32 +165,102 @@ class SegmentTabletop {
     marker_array.markers.resize(cluster_indices.size());
     //std::cout<<cluster_indices.size() << " Objects Found.."<<std::endl;
     
-    
     int i = 0;
     for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it) {
       pcl::PointCloud<pcl::PointXYZRGB>::Ptr object_cluster (new pcl::PointCloud<pcl::PointXYZRGB>);
-      float avg_r = 0, avg_g = 0, avg_b = 0, counter = 0;
-      
+      float avg_r = 0, avg_g = 0, avg_b = 0, counter = 0, z_avg = 0;
+
+      geometry_msgs::PointStamped base_temp;
+      geometry_msgs::PointStamped camera_temp;
+      std::vector<float> x_vals;
+      std::vector<float> y_vals;
 
       for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit){
+        
         // Summing up rgb values of points in object_cluster point clouds
         avg_r += objects->points[*pit].r;
         avg_g += objects->points[*pit].g;
-        avg_b += objects->points[*pit].b;  
-
-        object_cluster->points.push_back (objects->points[*pit]); //*
-        
+        avg_b += objects->points[*pit].b; 
         counter++;
+
+        object_cluster->points.push_back (objects->points[*pit]); 
+        
+        camera_temp.header.frame_id = input_cloud.header.frame_id;
+        camera_temp.point.x = objects->points[*pit].x;
+        camera_temp.point.y = objects->points[*pit].y;
+        camera_temp.point.z = objects->points[*pit].z;
+
+     	//transform point to base_link frame
+     	listener.transformPoint("base_link",camera_temp,base_temp);
+
+     	x_vals.push_back(base_temp.point.x); 
+		y_vals.push_back(base_temp.point.y);
+		z_avg += base_temp.point.z; 
       }
 
-      // Finding average rgb values of cylinders and avg z value
-      avg_r = avg_r/counter, avg_g = avg_g/counter, avg_b = avg_b/counter;
+      // Finding average rgb values and z value of cylinder
+      avg_r = avg_r/counter, avg_g = avg_g/counter, avg_b = avg_b/counter, z_avg = z_avg/counter;
+
+      // Fitting ellipse to cirular top of cylinder
+      Eigen::MatrixXd D_mat (x_vals.size(),6);
+      Eigen::MatrixXd D_mat_trans (6, x_vals.size());
+      Eigen::MatrixXd S_mat (6, 6);
+      Eigen::MatrixXd C_mat = Eigen::MatrixXd::Constant(6, 6, 0);
+
+      // D = [x.*x, x.*y, y.*y, x, y, ones];
+      for(int i=0; i<x_vals.size(); i++){
+      	D_mat(i,0) = x_vals[i]*x_vals[i];
+      	D_mat(i,1) = x_vals[i]*y_vals[i];
+      	D_mat(i,2) = y_vals[i]*y_vals[i];
+      	D_mat(i,3) = x_vals[i];
+      	D_mat(i,4) = y_vals[i];
+      	D_mat(i,5) = 1.0;
+      }
       
+      D_mat_trans = D_mat.transpose();
+      S_mat = D_mat_trans * D_mat;
+
+      // Build 6x6 constraint matrix
+      C_mat(5,5) = 0; C_mat(0,2) = 2; C_mat(1,1) = -1; C_mat(2,0) = 2;
+
+      // Solving eigensystem
+      Eigen::EigenSolver< Eigen::MatrixXd > es(S_mat.inverse() * C_mat);
+      // Finding positive eigenvalue
+      int posEigen;
+      for(int i=0; i<es.eigenvalues().size(); i++){
+      	if(es.eigenvalues().real()[i]>0.01){
+      		posEigen = i;
+      		//std::cout << "\nEigenvalue: ";
+	  		//std::cout << es.eigenvalues().real()[i];
+	  		//std::cout << "\nEigenvectors: ";
+	  		//std::cout << es.eigenvectors().real().col(i);
+      	}
+      }
+      
+      // Finding the centre of the fitted ellipse
+      float a,b,c,d,f, g, x0, y0, R = 0;
+      a = es.eigenvectors().real().col(posEigen)[0];
+      b = es.eigenvectors().real().col(posEigen)[1]/2.0;
+      c = es.eigenvectors().real().col(posEigen)[2];
+      d = es.eigenvectors().real().col(posEigen)[3]/2.0;
+      f = es.eigenvectors().real().col(posEigen)[4]/2.0;
+      g = es.eigenvectors().real().col(posEigen)[5];
+      x0=(c*d-b*f)/(b*b-a*c);
+      y0=(a*f-b*d)/(b*b-a*c);
+
+      //Finding radius of cylinder
+      for(int i=0; i<x_vals.size(); i++){
+      	R += (x0 - x_vals[i])*(x0 - x_vals[i]) + (y0 - y_vals[i])*(y0 - y_vals[i]);
+      }
+      R = R/x_vals.size();
+      R = sqrt(R);
+
+	  
       object_cluster->width = object_cluster->points.size ();
       object_cluster->height = 1;
       object_cluster->is_dense = true;
 
-      
+      /*
       pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> normalEstimation;
       pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
       normalEstimation.setInputCloud(object_cluster);
@@ -209,25 +277,27 @@ class SegmentTabletop {
       Eigen::Vector4f centroid;
       pcl::compute3DCentroid(*object_cluster, centroid);
       crh.setCentroid(centroid);
-	
+	  */
 
       geometry_msgs::PointStamped Centroid_Camera_Link;
       geometry_msgs::PointStamped Centroid_Base_Link;
 
-
       Centroid_Camera_Link.header.frame_id = input_cloud.header.frame_id;
-      Centroid_Camera_Link.point.x = centroid[0];
-      Centroid_Camera_Link.point.y = centroid[1];
-      Centroid_Camera_Link.point.z = centroid[2];
+      //Centroid_Camera_Link.point.x = centroid[0];
+      //Centroid_Camera_Link.point.y = centroid[1];
+      //Centroid_Camera_Link.point.z = centroid[2];
+      
       //transform point to base_link frame
       listener.transformPoint("base_link",Centroid_Camera_Link,Centroid_Base_Link);
 
-
+      Centroid_Base_Link.point.x = x0;
+      Centroid_Base_Link.point.y = y0;
+      Centroid_Base_Link.point.z = z_avg;
 
       float X_pos = Centroid_Base_Link.point.x;
       float Y_pos = Centroid_Base_Link.point.y;
       float Z_pos = Centroid_Base_Link.point.z;
-      //filter out anything that could be the base of the robot or the gripper
+      // filter out anything that could be the base of the robot or the gripper
       if (sqrt(X_pos*X_pos+Y_pos*Y_pos+Z_pos*Z_pos) > filter_base_link_radius ){
         
         result_.centroids.push_back(Centroid_Base_Link);
@@ -236,10 +306,13 @@ class SegmentTabletop {
         marker_array.markers[i].header.stamp = ros::Time();
         //marker_array.markers[i].ns = "my_namespace";
         marker_array.markers[i].id = i;
-        marker_array.markers[i].type = visualization_msgs::Marker::SPHERE;
+        marker_array.markers[i].type = visualization_msgs::Marker::CYLINDER;
         marker_array.markers[i].action = visualization_msgs::Marker::ADD;
         
-        // Positioning markers 
+
+        /*
+        // Positioning markers - linear method (not super accurate)
+        float cylinder_offset = 0.025;
         float x_offset = X_pos * (cylinder_offset/sqrt(X_pos*X_pos+Y_pos*Y_pos));
         float y_offset = Y_pos * (cylinder_offset/sqrt(X_pos*X_pos+Y_pos*Y_pos));
         marker_array.markers[i].pose.position.x = X_pos - x_offset;
@@ -249,14 +322,18 @@ class SegmentTabletop {
         else{
         	marker_array.markers[i].pose.position.y = Y_pos + y_offset;
         }
-		
+		*/
+        
+        marker_array.markers[i].pose.position.x = X_pos;
+        marker_array.markers[i].pose.position.y = Y_pos;
+        marker_array.markers[i].pose.position.z = Z_pos;
         marker_array.markers[i].pose.orientation.x = 0.0;
         marker_array.markers[i].pose.orientation.y = 0.0;
         marker_array.markers[i].pose.orientation.z = 0.0;
         marker_array.markers[i].pose.orientation.w = 1.0;
-        marker_array.markers[i].scale.x = 0.05;
-        marker_array.markers[i].scale.y = 0.05;
-        marker_array.markers[i].scale.z = 0.05;
+        marker_array.markers[i].scale.x = 2*R;
+        marker_array.markers[i].scale.y = 2*R;
+        marker_array.markers[i].scale.z = 0.12;//4*Z_pos;
         marker_array.markers[i].color.a = 1.0;
         //ROS_INFO("This is centroid number %.4f", i);
         
